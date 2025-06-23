2afefe1685c37ade91f76a7beb0b52bc7cb01f80
ttzytt <Zyt2006613@outlook.com>
2025-06-24 02:07:24 +0800

    change server addr

--- examples/bench_client.rs
+++ examples/bench_client.rs
@@ -8,9 +8,16 @@ fn main(){
     tracing_subscriber::fmt::init();
     unsafe { env::set_var("RUST_BACKTRACE", "full") };
 
+    // let mut client = BenchClientManager::new(
+    //     "localhost:12345",
+    //     "localhost:12346"
+    // );
+
     let mut client = BenchClientManager::new(
-        "localhost:12345",
-        "localhost:12346"
+        "130.245.173.102:12345",
+        "130.245.173.102:12346"
     );
+
+
     client.run().unwrap();
 }
